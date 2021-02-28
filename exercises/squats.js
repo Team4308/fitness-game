b7428fd284d9693c54ebbaa0b3b2d456ad64e9c4
Aayush Panda <aayush.vinayak@gmail.com>
2021-02-28 18:37:21 -0500

    squat detection

--- exercises/squats.js
+++ exercises/squats.js
@@ -1,18 +1,13 @@
 var high = true;
 var nsquats = 0;
 var height;
-<<<<<<< Updated upstream
 var first = true;
-=======
-var first;
->>>>>>> Stashed changes
 
 export default function (landmarks) {
     var right = landmarks[0].y - landmarks[28].y;
     var left = landmarks[0].y - landmarks[27].y;
     var dist = (right + left) / 2;
 
-<<<<<<< Updated upstream
     if(first){
         height = dist-0.05;
     }
@@ -20,10 +15,6 @@ export default function (landmarks) {
     if (dist < 0.2 && high) {
         nsquats++;
         high = false;
-=======
-    
-
->>>>>>> Stashed changes
     }
 
     if (disty > height && !high) {
