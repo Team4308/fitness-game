7ab82ce0a28285ffd55b5089fe164cc132114bfb
Woze Parrot <wozeparrot@gmail.com>
2021-02-28 17:33:09 -0500

    dynamic loading

---
+++ exercises/pushups.js
@@ -0,0 +1,28 @@
+// Pushup detection
+const pushupCtr = document.getElementById('p_bar');
+
+var total_pushups = 0;
+var npushups = 0;
+var pushuphigh = false;
+
+export default function(landmarks) {
+    var nosepos = landmarks[0].y;
+    var meanhpos = (landmarks[15].y + landmarks[16].y) / 2;
+    var nosehanddist = meanhpos - nosepos;
+
+    // High state
+    if (nosehanddist > 0.5 && pushuphigh == false) {
+        npushups++;
+        pushuphigh = true;
+        total_pushups = Math.floor(npushups / 2);
+    }
+
+    // Low state
+    if (nosehanddist < 0.2 && pushuphigh == true) {
+        npushups++;
+        pushuphigh = false;
+        total_pushups = Math.floor(npushups / 2);
+    }
+
+    return [1, total_pushups];
+}
--- tracking.js
+++ tracking.js
@@ -1,48 +1,57 @@
+// Main tracking file. Loads exersices dynamically
+
+// Grab elements for use with mediapipe
 const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
-const pushupCtr = document.getElementById('p_bar')
-var initialising = 1
-var npushups = 0;
-var pushuphigh = false;
-var nosepos;
-var meanhpos;
-var nosehandist;
 
+// Detection Function. Changed Dynamically
+var detectFn = (landmarks) => {
+    return [0, 0];
+};
+
+function loadExercise(name) {
+    import("./exercises/" + name + ".js").then(obj => detectFn = obj.default).catch(err => console.error(err));
+}
+
+// Post Detect Callback
+var postDetect = (detects) => {};
+
+function setPostDetect(callback) {
+    postDetect = callback;
+}
+
+// Resulting Detection
+var detects = [0, 0];
+
+function getDetects() {
+    return detects;
+}
+
+function getDetectsAccuracy() {
+    return detects[0];
+}
+
+function getDetectsCount() {
+    return detects[1];
+}
+
+// Pose estimation callback
 function onResults(results) {
+    // Draw trackers onto canvas
     canvasCtx.save();
     canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
     canvasCtx.drawImage(
         results.image, 0, 0, canvasElement.width, canvasElement.height);
     drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
         {color: '#00FF00', lineWidth: 4});
-    drawLandmarks(canvasCtx, results.poseLandmarks,
-        {color: '#FF0000', lineWidth: 2});
     canvasCtx.restore();
 
-
-    // Pushup detection
-    
-    nosepos = results.poseLandmarks[0].y
-    meanhpos = (results.poseLandmarks[15].y + results.poseLandmarks[16].y) / 2
-
-    nosehandist = meanhpos - nosepos
-
-    if (nosehandist > 0.5 && pushuphigh == false) {
-        npushups++;
-        console.log(Math.floor(npushups / 2));
-        pushuphigh = true;
-        pushupCtr.innerHTML = Math.floor(npushups / 2)
-    }
-
-    if (nosehandist < 0.2 && pushuphigh == true) {
-        npushups++;
-        console.log(Math.floor(npushups / 2));
-        pushuphigh = false;
-        pushupCtr.innerHTML = Math.floor(npushups / 2)
-    }
+    detects = detectFn(results.poseLandmarks);
+    postDetect(detects);
 }
 
+// Mediapipe pose estimation
 const pose = new Pose({
     locateFile: (file) => {
         return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
@@ -56,6 +65,7 @@ pose.setOptions({
 });
 pose.onResults(onResults);
 
+// Start camera
 const camera = new Camera(videoElement, {
     onFrame: async () => {
         await pose.send({image: videoElement});
@@ -64,4 +74,3 @@ const camera = new Camera(videoElement, {
     height: 720
 });
 camera.start();
-
